edbdc9ccd0314b12b544a2e8a95d8dd31972f7a8
StarPlatinu <thanhvp1509@gmail.com>
2023-03-28 16:06:32 +0700

    Update Boostrap cdn

--- assignment/src/main/java/com/lab/Controller/UserController.java
+++ assignment/src/main/java/com/lab/Controller/UserController.java
@@ -1,23 +1,27 @@
 package com.lab.Controller;
 
+import com.lab.Model.Role;
 import com.lab.Model.User;
+import com.lab.Repositories.RoleRepository;
 import com.lab.Services.UserNotFoundEXxception;
 import com.lab.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
 @Configurable
 public class UserController {
     @Autowired private UserService service;
+    @Autowired private RoleRepository roleRepository;
 
     @GetMapping("/users")
     public String showUserList(Model model){
@@ -28,7 +32,6 @@ public class UserController {
 
     @GetMapping("/users/new")
     public String showNewForm(Model model){
-
         model.addAttribute("user",new User());
         model.addAttribute("pageTitle","Add New User");
         return "user_from";
@@ -36,8 +39,13 @@ public class UserController {
 
     @PostMapping("/users/save")
     public String saveUser(User user, RedirectAttributes ra){
+        Role role = roleRepository.findByName("ROLE_ADMIN");
+        if(role == null){
+            role = checkRoleExist();
+        }
+        user.setRoles(Arrays.asList(role));
        service.save(user);
-       ra.addFlashAttribute("message","The user has been saved successfull.");
+       ra.addFlashAttribute("message","The user has been saved successfully.");
         return "redirect:/users";
     }
 
@@ -47,11 +55,49 @@ public class UserController {
             User user = service.get(id);
             model.addAttribute("user", user);
             model.addAttribute("pageTitle","Edit User (ID: "+id+")");
-            return "user_form";
+            return "user_from";
         } catch (UserNotFoundEXxception e) {
-            ra.addFlashAttribute("message","The user has been update successfull.");
+            ra.addFlashAttribute("message",e.getMessage());
             return "redirect:/users";
         }
+    }
 
+    @GetMapping("/users/delete/{id}")
+    public String delete(@PathVariable("id") Integer id,RedirectAttributes ra) {
+        try {
+        service.delete(id);
+        ra.addFlashAttribute("message","The user ID "+id+" has been deleted");
+        } catch (UserNotFoundEXxception e) {
+            ra.addFlashAttribute("message",e.getMessage());
+        }
+        return "redirect:/users";
+    }
+
+    // handler method to handle user registration form submit request
+    @PostMapping("/register/save")
+    public String registration(@ModelAttribute("user") User userDto,
+                               BindingResult result,
+                               Model model){
+        User existingUser = service.findUserByEmail(userDto.getEmail());
+        // encrypt the password using spring security
+
+        if(existingUser != null && existingUser.getEmail() != null && !existingUser.getEmail().isEmpty()){
+            result.rejectValue("email", null,
+                    "There is already an account registered with the same email");
+        }
+
+        if(result.hasErrors()){
+            model.addAttribute("user", userDto);
+            return "/register";
+        }
+
+        service.save(userDto);
+        return "redirect:/register?success";
+    }
+
+    private Role checkRoleExist(){
+        Role role = new Role();
+        role.setName("ROLE_ADMIN");
+        return roleRepository.save(role);
     }
 }
--- assignment/src/main/java/com/lab/Repositories/RoleRepository.java
+++ assignment/src/main/java/com/lab/Repositories/RoleRepository.java
@@ -1,2 +1,9 @@
-package com.lab.Repositories;public class RoleRepository {
+package com.lab.Repositories;
+
+import com.lab.Model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Role findByName(String name);
 }
